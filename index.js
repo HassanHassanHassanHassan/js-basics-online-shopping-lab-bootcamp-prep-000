9bd96140d5ca42e5cd28a5127055370981f8ba72
compiled-lab-0043 <chaudhy.hassan@yahoo.com>
2019-01-07 01:58:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,66 @@
 var cart = [];
 
 function getCart() {
- return cart;
-}
-
-function setCart(c) {
-  cart = c;
   return cart;
 }
 
+function setCart(newCart) {
+  cart = newCart;
+}
+
 function addToCart(item) {
- // write your code here
+  Math.floor(Math.random() * 100) + 1; 
+  var newItem = {itemName:item, itemPrice: Math.floor(Math.random() * 100) + 1 }
+  cart.push(newItem)
+  return `${item} has been added to your cart.`
 }
 
-function viewCart() {
-  // write your code here
+
+function viewCart(item) {
+if (cart.length === 0) {
+    return ("Your shopping cart is empty.")
+  } 
+else if (cart.length === 1) {
+    var OneItem = `In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`
+     return  `In your cart, you have ${item}`
 }
 
-function total() {
-  // write your code here
+
+}  
+
+
+function total(item) {
+   let cost = 0
+   for (let i = 0; cart.length > i; i++){
+    for (item in cart[i][item])
+    {t += cart[i][item]
+   }}
+     return cost
 }
 
-function removeFromCart(item) {
-  // write your code here
+function removeFromCart(item){
+let iteminCart = false
+
+for (let i = 0; cart.length > i; i++){
+if (cart[i].hasOwnProperty(item))
+{iteminCart = true}
+cart.splice(0, i)
 }
 
-function placeOrder(cardNumber) {
-  // write your code here
+if (false === iteminCart)
+return "That item is not in your cart."
 }
+
+function placeOrder(cardNumber){
+var zzz = "Sorry, we don't have a credit card on file for you."
+var xxx = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+if(cardNumber) {
+   setCart([]);
+ return xxx
+  }
+
+else
+{return zzz }
+}
+
+
